d6dbc06f8bcdf5980f916040e32a769b6ea9a2ec
user <user@users-MacBook-Pro.local>
2019-05-10 14:40:23 +0100

    hello

---
+++ hi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#This is a simple bash script that will poll github for changes to your repo,
+
+while true
+do
+
+git pull; 
+git checkout 2019-05-08-f9d607790d49484eaaa8dc134ad1005a;
+
+
+done
