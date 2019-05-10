ed54c752306c158a2df6e992412578d8397e05cf
Unicorn Rentals <elasticvolcano@gmail.com>
2019-05-10 09:01:51 +0000

    Added comma that was inadvertently removed in the last patch.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'e610214213c1f6abaa76ac00bf8afd25'
-__version__ = '72cae0825090e13f51f47f9af1d95233'
+__vcs_id__ = '33a809cc0d98cb5813c63fba04f865db'
+__version__ = 'f95469020404e8192628a024627dd18e'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
